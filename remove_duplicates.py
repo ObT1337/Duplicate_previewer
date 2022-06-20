ef14d370974d61389e103a6e33cdfaa793d1a86c
ObT1337 <a12048115@unet.univie.ac.at>
2022-06-20 10:49:56 +0200

    Applied pyQT
    TODO: CHange Layouts to stacked Ui to easily switch between them.

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -2,45 +2,57 @@ import argparse
 import glob
 import os
 import sys
-import threading
-import tkinter as tk
-from multiprocessing import parent_process
-from tkinter import messagebox, ttk
+from time import sleep
 
-import imageio
 import numpy as np
 from AppKit import NSScreen
-from PIL import Image, ImageTk
-from pillow_heif import register_heif_opener
+from PyQt5.QtCore import QCoreApplication, QDir, QEvent, Qt, QUrl, pyqtSignal
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
+from PyQt5.QtMultimediaWidgets import QVideoWidget
+from PyQt5.QtWidgets import (
+    QAction,
+    QApplication,
+    QFileDialog,
+    QHBoxLayout,
+    QLabel,
+    QMainWindow,
+    QPushButton,
+    QSizePolicy,
+    QSlider,
+    QStyle,
+    QVBoxLayout,
+    QWidget,
+)
 
 
-class Preview_Window(tk.Tk):
-    def __init__(self, SCALE_FACTOR=1, title="Preview Window") -> None:
-        super().__init__()
+class PyQt_App(QApplication):
+    def __init__(self, *args, childs) -> None:
+        super().__init__(*args)
+        self.childs = childs
+
+    def mainloop(self):
+        while len(self.childs > 0):
+            if not self.childs.is_alive():
+                self.childs.pop()
+        sys.exit(self.exec_())
+
+
+class Preview_Window(QMainWindow):
+    def __init__(
+        self, app, SCALE_FACTOR=1, title="Preview Window", parent=None
+    ) -> None:
+        super(Preview_Window, self).__init__(parent)
+        self.app = app
+        self.setWindowTitle(title)
         self.SCALE_FACTOR = SCALE_FACTOR
-        self.title = title
-        self.wait_var = tk.IntVar()
-        self.task = tk.IntVar()
-        self.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.set_buttons()
+        self.wait_variable = 0
+        # self.task = tk.IntVar()
+        # self.protocol("WM_DELETE_WINDOW", self.on_closing)
         print("Setting up Window")
         self.set_window_size()
-        print("Setting up Buttons")
-        self.set_buttons()
-        print("setting default images")
-        self.set_default_img_content()
-        print("packing content")
-        self.pack_content()
         print("Ready...")
-        self.update()
-        self.sub_program = None
-
-    def set_buttons(self):
-        self.remove_button = tk.Button(
-            self, text="Remove", command=self.remove_src, width=20
-        )
-        self.continue_button = tk.Button(
-            self, text="Continue", command=self.next_preview, width=20
-        )
 
     def set_window_size(
         self,
@@ -49,88 +61,162 @@ class Preview_Window(tk.Tk):
         self.screen_h = NSScreen.mainScreen().frame().size.height
         self.size_x = self.screen_w / self.SCALE_FACTOR
         self.size_y = self.screen_h / self.SCALE_FACTOR
-        offset_x = int(self.screen_w - self.size_x) // 2
-        offset_y = int(self.screen_h - self.size_y) // 2
-        self.geometry("%dx%d+%d+%d" % (self.size_x, self.size_y, offset_x, offset_y))
+        self.offset_x = int(self.screen_w - self.size_x) // 2
+        self.offset_y = int(self.screen_h - self.size_y) // 2
+        self.resize(self.size_x, self.size_y)
         self.grid_size_x = int(self.size_x // 10)
         self.grid_size_y = int(self.size_y // 10)
         self.content_w = int(self.grid_size_x * 4.5)
         self.content_h = int(self.grid_size_y * 4.5)
         self.content_size = (self.content_w, self.content_h)
 
-    def set_default_img_content(self):
-        default = Image.new(mode="RGB", size=self.content_size)
-        default = ImageTk.PhotoImage(default)
-        self.src_panel = tk.Label(
-            self,
-            image=default,
-            width=self.content_w,
-            height=self.content_h,
-            background="black",
-        )
-        self.dest_panel = tk.Label(
-            self,
-            image=default,
-            width=self.content_w,
-            height=self.content_h,
-            background="black",
-        )
+    def set_buttons(self):
+        self.remove_button = QPushButton()
+        self.remove_button.setEnabled(True)
+        self.remove_button.setText("Remove")
+        self.remove_button.clicked.connect(self.remove_file)
+        self.continue_button = QPushButton()
+        self.continue_button.setEnabled(True)
+        self.continue_button.setText("Continue")
+        self.continue_button.clicked.connect(self.next_preview)
 
-    def pack_content(self):
-        button_height = self.remove_button.winfo_reqheight()
-        button_width = self.remove_button.winfo_reqwidth()
-        panel_x = self.grid_size_x // 3
-        panel_y = self.grid_size_y // 3
-        middle = panel_x + self.content_w + (panel_x - button_width) // 2
-        button_x, button_y = (middle, panel_y + self.content_h + button_height)
-        self.src_panel.place(x=panel_x, y=panel_y)
-        self.dest_panel.place(x=2 * panel_x + self.content_w, y=panel_y)
-        self.remove_button.place(x=button_x, y=button_y)
-        self.continue_button.place(
-            x=button_x,
-            y=button_y + button_height * 1.5,
-        )
+    def set_img_content(self, src, dest):
+        self.src_img = QLabel()
+        self.dest_img = QLabel()
 
-    def remove_src(self):
-        self.task.set(1)
-        self.next_preview()
+        self.src_img.resize(self.content_w, self.content_h)
+        self.dest_img.setAlignment(Qt.AlignCenter)
+        self.src_img.setAlignment(Qt.AlignCenter)
+        self.dest_img.resize(self.content_w, self.content_h)
+
+        # preview layout
+        preview = QHBoxLayout()
+        preview.addWidget(self.src_img)
+        preview.addWidget(self.dest_img)
+
+        self.pack_content(preview)
+
+        for img, target in zip([src, dest], [self.src_img, self.dest_img]):
+            self.openImage(img, target)
+
+    def unload_content(self):
+        pass
+        # if "src_img" in self.__dict__.keys():
+        #     print("Unloading Image")
+        #     self.src_img = None
+        #     self.dest_img = None
+        if "src_video" in self.__dict__.keys():
+            print("Unloading Video")
+            for obj in [
+                self.src_video,
+                self.dest_video,
+            ]:
+                obj.stop()
+            self.src_img.close()
+            self.dest_img.close()
+            self.src_widget = QVideoWidget()
+            self.dest_widget = QVideoWidget()
+        self.pack_content(None)
+
+    def set_video_content(self, src, dest):
+        self.src_video = QMediaPlayer(None, QMediaPlayer.VideoSurface)
+        self.dest_video = QMediaPlayer(None, QMediaPlayer.VideoSurface)
+        self.src_widget = QVideoWidget()
+        self.dest_widget = QVideoWidget()
+
+        # preview layout
+        preview = QHBoxLayout()
+        preview.addWidget(self.src_widget)
+        preview.addWidget(self.dest_widget)
+
+        self.src_video.setVideoOutput(self.src_widget)
+        self.dest_video.setVideoOutput(self.dest_widget)
+
+        self.pack_content(preview)
+
+        for video, target in zip([src, dest], [self.src_video, self.dest_video]):
+            self.openVideo(video, target)
+            self.playVideo(target)
+
+    def pack_content(self, preview):
+        # Create a widget for window contents
+
+        self.wid = QWidget(self)
+        self.setCentralWidget(self.wid)
+
+        # Buttons
+        buttonLayout = QHBoxLayout()
+        buttonLayout.addWidget(self.remove_button)
+        buttonLayout.addWidget(self.continue_button)
+
+        # set layout
+        layout = QVBoxLayout()
+        if preview is not None:
+            layout.addLayout(preview)
+        layout.addLayout(buttonLayout)
+
+        self.wid.setLayout(layout)
+
+    def openImage(self, fileName, target):
+        if fileName != "":
+            print(f"Setting Image to {fileName}")
+            pixmap = QPixmap(fileName)
+            if pixmap.width() > pixmap.height():
+                pixmap = pixmap.scaledToWidth(self.content_w)
+                print("rescaling width")
+            else:
+                pixmap = pixmap.scaledToHeight(self.content_h)
+                print("rescaling height")
+            target.setPixmap(pixmap)
+
+    def openVideo(self, fileName, target):
+        if fileName != "":
+            print(f"Setting Media to {fileName}")
+            target.setMedia(QMediaContent(QUrl.fromLocalFile(fileName)))
+
+    def playVideo(self, mediaPlayer):
+        print("Starting Video")
+        mediaPlayer.play()
+
+    def exitCall(self):
+        sys.exit(self.app.exec_())
+
+    def remove_file(self):
+        self.wait_variable = 2
 
     def next_preview(self):
-        self.wait_var.set(0)
+        self.wait_variable = 1
 
-    def on_closing(self):
-        if messagebox.askokcancel("Quit", "Do you want to quit?"):
-            self.withdraw()
-            print("trying to close SubProgram")
-            self.wait_var.set(-1)
-
-    def wait_continue(self):
-        self.update()
-        self.next_preview()
-        print("Waiting for user to push button")
-        self.wait_variable(self.wait_var)
-        print("Variable Changed!")
-        if self.wait_var.get() == -1:
-            print("User pressed cancel")
-            os.kill(os.getpid(), 9)
-        if self.task.get() == 1:
+    def wait_for_input(self):
+        while not self.wait_variable:
+            if QCoreApplication.closingDown():
+                QCoreApplication.exit()
+            QCoreApplication.processEvents()
+            sleep(0.1)
+        if self.wait_variable == 2:
             print("removing")
-            print("User pressed remove")
-            # os.remove(file)
         else:
-            print("User pressed continue")
-            print("continuing", self.task)
-        self.task.set(0)
+            print("continuing")
+        self.wait_variable = 0
 
+    def mediaStateChanged(self, state, mediaPlayer):
+        if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
+            self.playButton.setIcon(self.style().standardIcon(QStyle.SP_MediaPause))
+        else:
+            self.playButton.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
 
-def stream(label: tk.Label, video, content_size):
-    video = np.array([im for im in video.iter_data()], dtype=np.uint8)
-    while True:
-        for image in video:
-            image_frame = Image.fromarray(image)
-            image_frame.thumbnail(content_size, Image.ANTIALIAS)
-            frame_image = ImageTk.PhotoImage(image_frame)
-            label.configure(image=frame_image)
+    def positionChanged(self, position):
+        self.positionSlider.setValue(position)
+
+    def durationChanged(self, duration):
+        self.positionSlider.setRange(0, duration)
+
+    def setPosition(self, position):
+        self.mediaPlayer.setPosition(position)
+
+    def handleError(self):
+        self.playButton.setEnabled(False)
+        self.errorLabel.setText("Error: " + self.mediaPlayer.errorString())
 
 
 def argparser():
@@ -143,78 +229,22 @@ def argparser():
 
 
 def preview_image(file, dest_file, app: Preview_Window):
-    print(f"Current file: {file}")
-    src_img = Image.open(file)
-    dest_img = Image.open(dest_file)
-    src_img.thumbnail(app.content_size, Image.ANTIALIAS)
-    dest_img.thumbnail(app.content_size, Image.ANTIALIAS)
-    src_img = ImageTk.PhotoImage(src_img)
-    dest_img = ImageTk.PhotoImage(dest_img)
-    app.src_panel["image"] = src_img
-    app.dest_panel["image"] = dest_img
-    app.wait_continue()
+    app.unload_content()
+    app.set_img_content(file, dest_file)
+    app.wait_for_input()
 
 
 def preview_video(file, dest_file, app: Preview_Window):
-    print(file)
-    src_video = imageio.get_reader(file)
-    dest_video = imageio.get_reader(dest_file)
-
-    src_thread = KillableThread(
-        target=stream, args=(app.src_panel, src_video, app.content_size)
-    )
-    dest_thread = KillableThread(
-        target=stream, args=(app.dest_panel, dest_video, app.content_size)
-    )
-
-    src_thread.daemon = 1
-    dest_thread.daemon = 1
-    src_thread.start()
-    dest_thread.start()
-    app.wait_continue()
-    src_thread.kill()
-    dest_thread.kill()
+    app.unload_content()
+    app.set_video_content(file, dest_file)
+    app.wait_for_input()
 
 
-class KillableThread(threading.Thread):
-    def __init__(self, *args, **keywords):
-        threading.Thread.__init__(self, *args, **keywords)
-        self.killed = False
-
-    def start(self):
-        self.__run_backup = self.run
-        self.run = self.__run
-        threading.Thread.start(self)
-
-    def __run(self):
-        sys.settrace(self.globaltrace)
-        self.__run_backup()
-        self.run = self.__run_backup
-
-    def globaltrace(self, frame, event, arg):
-        if event == "call":
-            return self.localtrace
-        else:
-            return None
-
-    def localtrace(self, frame, event, arg):
-        if self.killed:
-            if event == "line":
-                raise SystemExit()
-        return self.localtrace
-
-    def kill(self):
-        self.killed = True
-
-
-class DataIterator(threading.Thread):
-    def __init__(self, source, dest, app, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
+class DataIterator:
+    def __init__(self, source, dest, app) -> None:
         self.source = source
         self.dest = dest
         self.app = app
-        self.pid = os.getpid()
-        print("Thread is Ready..")
 
     def stop(self):
         exit()
@@ -232,8 +262,10 @@ class DataIterator(threading.Thread):
                         to_del[file] = []
                     to_del[file].append(dest_file)
                     if filename.endswith(".HEIC"):
+                        print(f"Opening {file}")
                         preview_image(file, dest_file, self.app)
                     if filename.endswith(".MOV"):
+                        print(f"Opening {file}")
                         preview_video(file, dest_file, self.app)
 
 
@@ -246,15 +278,19 @@ def iterate_data(args, app):
 
 
 def main():
-    register_heif_opener()
+    # register_heif_opener()
     args = argparser()
-    app = Preview_Window(4)
-    app.after(10, iterate_data, args, app)
-    app.mainloop()
     # os.remove(file)
     # print(len(to_del))
     # for k, v in to_del.items():
     #     print(f"{k},{v}")
+    app = PyQt_App(sys.argv, childs=None)
+    preview_window = Preview_Window(app, 1)
+    app.childs = [preview_window]
+    preview_window.show()
+    data_iterator = DataIterator(args.source, args.dest, preview_window)
+    data_iterator.run()
+    # app.mainloop()
 
 
 if __name__ == "__main__":
